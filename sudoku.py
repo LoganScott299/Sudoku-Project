7b14f3b33c4d3594474fc2e03f36359b475e44ba
abalyberdin203 <arkasha.balyberdin@gmail.com>
2024-12-07 02:11:22 -0500

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -3,53 +3,76 @@ from board import Board
 
 pygame.init()
 
-button_color = (107, 2, 56)
+button_color = "dark orange"
 
-sudoku_background = pygame.image.load('sudoku.jpg')
-
-def draw_title(screen, x, y, text, title_color, outline_color):
-    font = "impact, arial"
-    button_font = pygame.font.SysFont(font, 64)
-    button_text = button_font.render(text, True, title_color)
-    button_outline_font = pygame.font.SysFont(font, 65)
-    button_outline_text = button_outline_font.render(text, True, outline_color)
-    screen.blit(button_outline_text, (x + (200 - button_outline_text.get_width()) // 2, y + (50 - button_outline_text.get_height()) // 2))
-    screen.blit(button_text, (x + (200 - button_text.get_width()) // 2, y + (50 - button_text.get_height()) // 2))
-
-def draw_button(screen, x, y, text, color):
-    button_font = pygame.font.SysFont("verdana, arial", 32)
+def draw_button(screen, x, y, text, color, border_color="dark orange", border_thickness = 5, gap_thickness = 5):
+    button_font = pygame.font.SysFont("Comic Sans", 21)
     button_text = button_font.render(text, True, (255,255,255))
-    pygame.draw.rect(screen, color, (x,y,200,50),border_radius = 15)
-    screen.blit(button_text, (x + (200 - button_text.get_width()) // 2, y + (50 - button_text.get_height()) // 2))
+    pygame.draw.rect(screen, color, (x,y,100,40))
+    pygame.draw.rect(screen, border_color, (x - gap_thickness - border_thickness, y - gap_thickness - border_thickness, 100 + 2 * (gap_thickness + border_thickness) , 40+2*(gap_thickness + border_thickness),
+                                            ),
+                     border_thickness
+                     )
+    pygame.draw.rect(screen, "black", (x - 5 - 1, y - 5 - 1, 100 + 2 * 5+2, 40 + 2 * 5+2,), 1)
+    pygame.draw.rect(screen, "black", (x - 5 - 1 - 3, y - 5 - 1 - 3, 100 + 2 * 6 + 6, 40 + 2 * 6 + 6,), 1)
+    screen.blit(button_text, (x + (105 - button_text.get_width()) // 2, y + (40 - button_text.get_height()) // 2))
 
-def draw_small_button(screen, x, y, text, color):
-    button_font = pygame.font.SysFont("arial", 22)
+def draw_small_button(screen, x, y, text, color, border_color = "dark orange", border_thickness = 5, gap_thickness = 4):
+    button_font = pygame.font.SysFont("Comic Sans", 16)
     button_text = button_font.render(text, True, (255,255,255))
-    pygame.draw.rect(screen, color, (x,y,80,40), border_radius = 15)
-    screen.blit(button_text, (x + 10, y + 6))
+    pygame.draw.rect(screen, color, (x,y,90,35))
+    pygame.draw.rect(screen, border_color, (x - gap_thickness - border_thickness, y - gap_thickness - border_thickness,
+                                            90 + 2 * (gap_thickness + border_thickness),
+                                            35 + 2 * (gap_thickness + border_thickness),
+                                            ),
+                     border_thickness
+                     )
+    pygame.draw.rect(screen, "black", (x - 4 - 1, y - 4 - 1, 90 + 2 * 4 + 2, 35 + 2 * 4 + 2,), 1)
+    pygame.draw.rect(screen, "black", (x - 4 - 1 - 3, y - 4 - 1 - 3, 90 + 2 * 5 + 6, 35 + 2 * 5 + 6,), 1)
+    screen.blit(button_text, (x + 11, y + 5))
 
 def main():
     try:
-        screen = pygame.display.set_mode((540, 600)) #540x600 Screen
+        screen = pygame.display.set_mode((540, 600))
+        welcome_title_font = pygame.font.Font(None, 70)
+        select_title_font = pygame.font.Font(None, 50)
+        game_over_title_font = pygame.font.Font(None, 70)
+        game_won_title_font = pygame.font.Font(None, 70)
+        #540x540 Screen
+        background_image = pygame.image.load('sudoku_1.jpg')
+        background_image = pygame.transform.scale(background_image, (540, 600))
+        welcome_surface = welcome_title_font.render("Welcome to Sudoku", 0, "red")
+        welcome_rectangle = welcome_surface.get_rect(
+            center=(540 // 2, 540 // 2 - 170)
+        )
+        select_surface = select_title_font.render("Select Game Mode:", 0, "red")
+        select_rectangle = select_surface.get_rect(
+            center=(540 // 2, 540 // 2 - 50)
+        )
+        game_over_surface = game_over_title_font.render("Game Over :(", 0, "red")
+        game_over_rectangle = game_over_surface.get_rect(
+            center=(540 // 2, 540 // 2 - 160)
+        )
+        game_won_surface = game_won_title_font.render("Game Won!", 0, "red")
+        game_won_rectangle=game_won_surface.get_rect(
+            center = (540 // 2, 540 // 2 - 160)
+        )
         pygame.display.set_caption("Sudoku")
         clock = pygame.time.Clock()
         running = True
-
         removed_cells = None
         board = None
         selected = None
 
         #Game loop for menu
         while running and not board:
-            screen.blit(sudoku_background, (0,0)) #Sudoku background
-            
-            #Title
-            draw_title(screen, 170, 70, "Play Sudoku", button_color, (0,0,0))
-            
+            screen.blit(background_image, (0,0)) #Custom Background
+            screen.blit(welcome_surface, welcome_rectangle)
+            screen.blit(select_surface, select_rectangle)
             #Easy, Medium, Hard Buttons
-            draw_button(screen, 170, 170, "Easy", button_color)
-            draw_button(screen, 170, 270, "Medium", button_color)
-            draw_button(screen, 170, 370, "Hard", button_color)
+            draw_button(screen, 50, 325, "EASY", button_color)
+            draw_button(screen, 220, 325, "MEDIUM", button_color)
+            draw_button(screen, 390, 325, "HARD", button_color)
 
             for event in pygame.event.get(): #Event listener
                 if event.type == pygame.QUIT: #Quit button
@@ -57,12 +80,12 @@ def main():
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     clickX, clickY = event.pos
-                    if 170 <= clickX <= 370:
-                        if 170 <= clickY <= 220:
+                    if 324 <= clickY <= 366:
+                        if 49 <= clickX <= 151:
                             removed_cells = 30 #Easy
-                        elif 270 <= clickY <= 320:
+                        elif 119 <= clickX <= 321:
                             removed_cells = 40 #Medium
-                        elif 370 <= clickY <= 420:
+                        elif 289 <= clickY <= 491:
                             removed_cells = 50 #Hard
 
                         if removed_cells:
@@ -82,12 +105,12 @@ def main():
 
                 if event.type == pygame.MOUSEBUTTONDOWN: #Select cell
                     x, y = event.pos
-                    if 550 <= y <= 590:
-                        if 50 <= x <= 130:
+                    if 551 <= y <= 588:
+                        if 100 <= x <= 192:
                             board.reset_to_original()
-                        elif 230 <= x <= 310:
+                        elif 225 <= x <= 317:
                             main()
-                        elif 410 <= x <= 490:
+                        elif 350 <= x <= 442:
                             exit()
                     else:
                         selected = board.click(x,y)
@@ -146,9 +169,9 @@ def main():
                 if board.is_full():
                     if board.check_board():
                         while running:
-                            screen.blit(sudoku_background, (0,0)) #Sudoku background
-                            draw_button(screen, 170, 150, "Game Won!", button_color)
-                            draw_small_button(screen, 230, 300, "   Exit", button_color)
+                            screen.blit(background_image, (0,0))
+                            screen.blit(game_won_surface, game_won_rectangle)
+                            draw_small_button(screen, 230, 300, "EXIT", button_color)
                             pygame.display.flip()
                             for event in pygame.event.get():  # Event listener
                                 if event.type == pygame.QUIT:  # Quit button
@@ -160,9 +183,9 @@ def main():
                                             exit()
                     if not board.check_board():
                         while running:
-                            screen.fill((255, 255, 255))
-                            draw_button(screen, 170, 150, "Game Over :(", button_color)
-                            draw_small_button(screen, 230, 300, "Restart", button_color)
+                            screen.blit(background_image, (0, 0))
+                            screen.blit(game_over_surface, game_over_rectangle)
+                            draw_small_button(screen, 230, 300, "RESTART", button_color)
                             pygame.display.flip()
                             for event in pygame.event.get():  # Event listener
                                 if event.type == pygame.QUIT:  # Quit button
@@ -175,9 +198,9 @@ def main():
 
             board.draw()
             #Reset, Restart, Exit Buttons
-            draw_small_button(screen, 50, 550, " Reset", button_color)
-            draw_small_button(screen, 230, 550, "Restart", button_color)
-            draw_small_button(screen, 410, 550, "   Exit", button_color)
+            draw_small_button(screen, 101, 552, " RESET", button_color)
+            draw_small_button(screen, 226, 552, "RESTART", button_color)
+            draw_small_button(screen, 351, 552, "  EXIT", button_color)
 
             pygame.display.flip()
             clock.tick(25)
